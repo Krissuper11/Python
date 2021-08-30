23fde1986c09367e37d85fa49ff86af03825c2ba
Kristofer Putsinin <kriput@ttu.ee>
2021-08-30 20:09:45 +0300

    My first commit

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,5 +1,5 @@
-nimi = input("What is your name? ")
-arv1 = int(input("Hello, " + nimi + "! Enter a random number: "))
+name = input("What is your name? ")
+arv1 = int(input("Hello, " + name + "! Enter a random number: "))
 arv2 = int(input("Great! Now enter a second random number: "))
 print(str(arv1) + " + " + str(arv2) + " is " + str(arv2 + arv1))
 
@@ -12,5 +12,4 @@ print("Roses are " + first_word + ",\n" + second_word + " are blue,\n" + "I love
 greeting = input("Enter a greeting: ")
 recipient = input("Enter a recipient: ")
 number = int(input("How many times to repeat: "))
-print((greeting + " " + recipient + "! ")*number)
-
+print((greeting + " " + recipient + "! ") * number)
