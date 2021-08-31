e4a9a47f5792f4296d32144ed3915af4a9c5a81a
Kristofer Putsinin <kriput@ttu.ee>
2021-08-31 12:42:20 +0300

    fixing style

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,3 +1,4 @@
+"""asks input and gives results"""
 name = input("What is your name? ")
 arv1 = int(input("Hello, " + name + "! Enter a random number: "))
 arv2 = int(input("Great! Now enter a second random number: "))
@@ -7,7 +8,8 @@ first_word = input("Roses are ..., ")
 second_word = input("... are blue, ")
 third_word = input("I love to ...")
 print("And so will you!\n")
-print("Roses are " + first_word + ",\n" + second_word + " are blue,\n" + "I love to " + third_word + "\nAnd so will you!")
+print("Roses are " + first_word + ",\n" + second_word + " are blue,\n" + "I love to " + third_word + "\nAnd so will "
+                                                                                                     "you!")
 
 greeting = input("Enter a greeting: ")
 recipient = input("Enter a recipient: ")
