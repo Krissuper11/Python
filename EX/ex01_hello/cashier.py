e899b5b96375a989b66b2b0af18b626b32d91b5f
Kristofer Putsinin <kriput@ttu.ee>
2021-09-01 23:20:04 +0300

    My commit

--- EX/ex01_hello/cashier.py
+++ EX/ex01_hello/cashier.py
@@ -1,22 +1,21 @@
 """Asks for sum and gives amount of coins needed."""
 sum = int(input("Enter a sum: "))  # reads input
 
-# counts how many 50 cent coins
+# counts how many 50 cent coins and what remains
 fifty_cent = sum // 50
-left = sum - 50 * fifty_cent
+left = sum % 50
 
-# counts how many 20 cent coins
+# counts how many 20 cent coins and what remains
 twenty_cent = left // 20
-left = left - 20 * twenty_cent
+left = left % 20
 
-# counts how many 10 cent coins
+# counts how many 10 cent coins and what remains
 ten_cent = left // 10
-left = left - ten_cent * 10
+left = left % 10
 
-# counts how many 5 cent coins
+# counts how many 5 cent coins and what remains
 five_cent = left // 5
-left = left - five_cent * 5
+left = left % 5
 
-one_cent = left   # all what left is 1 cent coin
-amount = fifty_cent + twenty_cent + ten_cent + five_cent + one_cent  # counts amount of coins
+amount = fifty_cent + twenty_cent + ten_cent + five_cent + left  # counts amount of coins
 print(f"Amount of coins needed: {amount}")  # prints the result
