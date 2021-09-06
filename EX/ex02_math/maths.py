215f0e59036c3ef9ab6d009be0c8fc9e0b6bbcc5
Kristofer Putsinin <kriput@ttu.ee>
2021-09-06 16:57:25 +0300

    My commit

--- EX/ex02_math/maths.py
+++ EX/ex02_math/maths.py
@@ -3,17 +3,14 @@
 
 def ects(ects, weeks):
     """Convert EAP to hour per week."""
-    if weeks < 1:
-        return False
+    if weeks < 1:  # can't divide by zero
+        return "Impossible!"
     else:
         result = ects * 26 / weeks
         if result < 168:  # 168 hours in week
             return result
         else:
             return "Impossible!"
-
-
-print(ects(1, 0))
 
 
 def average(a, b, c, d):
