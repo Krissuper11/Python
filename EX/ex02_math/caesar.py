4c0ff7d178ef2d4d0c4cc435eb3029fe8d3479d3
Kristofer Putsinin <kriput@ttu.ee>
2021-09-10 11:23:47 +0300

    Fixing isalpha

--- EX/ex02_math/caesar.py
+++ EX/ex02_math/caesar.py
@@ -5,10 +5,8 @@ def encode(message: str, shift: int) -> str:
     """Encode a message using a Caesar cipher."""
     new_letter = ""
     for i in range(len(message)):
-        if message[i] == " ":
-            new_letter += " "
-        elif message[i] == "'":
-            new_letter += "'"
+        if not message[i].isalpha():
+            new_letter += message[i]
         elif message[i].isalpha():
             index = ord(message[i]) + shift
             index -= 26 * ((index - 97) // 26)
@@ -16,3 +14,4 @@ def encode(message: str, shift: int) -> str:
         else:
             pass
     return new_letter
+print(encode("ab c ' _+", 3))
