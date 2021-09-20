710caf7429ed829240e85fa6834c694452b41ce5
Kristofer Putsinin <kriput@ttu.ee>
2021-09-20 17:08:37 +0300

    My commit

---
+++ EX/ex04_lists/lists.py
@@ -0,0 +1,22 @@
+"""List and tuple."""
+
+
+def generate_list(amount: int, data_type: str):
+    """Return a list with amount elements of type data_type."""
+    import random
+    some_list = []
+    if data_type == "int":
+        data_type = random.randint(0, 9)
+    elif data_type == "string":
+        data_type = "a"
+    elif data_type == "float":
+        data_type = random.random()
+    elif data_type == "list":
+        data_type = []
+    elif data_type == "tuple":
+        data_type = {}
+    elif data_type == "set":
+        data_type = set()
+    for i in range(amount):
+        some_list.append(data_type)
+    return some_list
