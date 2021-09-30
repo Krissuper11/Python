4bee96130ddb2a086bf3896346265824d79d4571
Kristofer Putsinin <kriput@ttu.ee>
2021-09-30 12:50:19 +0300

    My commit

---
+++ TK/tk2/exam.py
@@ -0,0 +1,17 @@
+"""."""
+
+
+def count_clumps(nums: list):
+    """."""
+    counter = 0
+    for i in range(len(nums)):
+        if i == 0:
+            if nums[i] == nums[i + 1]:
+                counter += 1
+            else:
+                continue
+        elif i == len(nums) - 1:
+            return counter
+        elif nums[i - 1] != nums[i] and nums[i] == nums[i + 1]:
+            counter += 1
+    return counter
