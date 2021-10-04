9768c15877ae0648f287edfcf1cf5a3be11d889a
Kristofer Putsinin <kriput@ttu.ee>
2021-10-04 15:57:35 +0300

    My commit

---
+++ EX/ex06_regex/easy.py
@@ -0,0 +1,123 @@
+"""Regex, yay!"""
+import re
+
+
+def find_words(text: str) -> list:
+    """
+    Given string text, return all the words in that string.
+
+    A word here is considered to be any combination letters that starts with
+    a capital letter and contains of at least one more lowercase letter.
+    Note that Estonian õ, ä, ö and ü should also be accepted here.
+
+    Words must be found using regex.
+
+    :param text: given string to find words from
+    :return: list of words found in given string
+    """
+    return re.findall("[A-Z][a-z]+", text)
+
+
+def find_words_with_vowels(text: str) -> list:
+    """
+    Given string text, return all the words in that string that start with a vowel.
+
+    A word here is considered to be any combination letters that starts with
+    a capital letter and contains of at least one more lowercase letter.
+    Note that Estonian õ, ä, ö and ü should also be accepted here.
+
+    Words must be found using regex.
+
+    :param text: given string to find words from
+    :return: list of words that start with a vowel found in given string
+    """
+    return re.findall("[AEIOUÕÄÖÜ][a-z]+", text)
+
+
+def find_sentences(text: str) -> list:
+    """
+    Given string text, return all sentences in that string.
+
+    A sentence always starts with a capital letter and ends with punctuation (.!?).
+    Note that a sentence may also contain all the typical symbols (like commas, colons, numbers, etc.).
+    A sentence may also end in multiple punctuation (example: "Wait...").
+
+    Sentences must be found using regex.
+
+    :param text: given string to find sentences from
+    :return: list of sentences found in given string
+    """
+    return re.findall("[A-ZÕÜÖÄ][a-zõüöä\-\,\: ]*[\.\!\?]*", text)
+
+
+def find_words_from_sentence(sentence: str) -> list:
+    """
+    Given a sentence, return all words in that sentence.
+
+    Here, a word is considered to be a normal word in a sentence,
+    that is separated from other words by a whitespace (or commas, etc.).
+    Note that numbers are also considered as words here, but commas, etc. are not
+    a part of a word.
+
+    Words must be found using regex.
+
+    :param sentence: given sentence to find words from
+    :return: list of words found in given sentence
+    """
+    word_list = re.findall("^[A-ZÕÜÖÄ][\w]*| [\wõüöä]*", sentence)
+    for i in range(len(word_list)):
+        if word_list[i][0] == " ":
+            word_list[i] = word_list[i][1:]
+    return word_list
+
+
+def find_words_from_sentences_only(text: str) -> list:
+    """
+    Given string text, return all words in that string that are a part of a sentence in that string.
+
+    A sentence is defined in function find_sentences().
+    A word is defined in function find_words_from_sentence().
+
+    :param text: given string to find words from
+    :return: list of words found in sentences from given string
+    """
+    pass
+
+
+def find_years(text: str) -> list:
+    """
+    Given string text, return a list of all 4-digit numbers (years) in that string.
+
+    Only 4-digit numbers are considered years here.
+    If there is a 5-digit number then that is not considered a year,
+    nor will it give two years. So you can not split them up.
+
+    Years must be found using regex.
+
+    Hint: use lookbehind and lookahead to check what comes before and after the numbers.
+
+    :param text: given string to find years from
+    :return: list of years (integers) found in given string
+    """
+    pass
+
+
+def find_phone_numbers(text: str) -> dict:
+    """
+    Given string text, return a dictionary of all the phone numbers in that text.
+
+    Phone number might be preceded by area code. Are code is a combination of plus sign and three numbers.
+    The phone number itself is a combination of 7-8 numbers.
+    The phone number might be separated from the area code with a whitespace, but not necessarily.
+
+    The function must return a dictionary where keys are the area codes
+    and values are lists of the phone numbers with the corresponding area number.
+    If a phone number does not have an area code given, its area code would be empty string,
+    so in dictionary it would be like that: {"": ["56332456"]}.
+
+    Phone numbers must be found using regex.
+
+    :param text: given string to find phone numbers from
+    :return: dict containing the numbers
+    """
+    pass
