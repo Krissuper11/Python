c5c01bad1105ee773c6965423dc67229e5a035e4
Kristofer Putsinin <kriput@ttu.ee>
2021-10-09 12:21:53 +0300

    My commit

--- EX/ex06_regex/schedule.py
+++ EX/ex06_regex/schedule.py
@@ -19,16 +19,17 @@ def create_schedule_string(input_string: str) -> str:
         task = match.group(3).lower()
         time = f"{int(hour):02}:{int(minute):02}"
         if time not in regex_dict:
-            regex_dict[time] = [task]
+            regex_dict[time] = task
         elif time in regex_dict:
-            regex_dict[time].append(task)
+            regex_dict[time] = f"{regex_dict[time]}, task"
     regex_dict = dict(sorted(regex_dict.items(), key=lambda x: x[0]))
     formatted_time = get_formatted_time(regex_dict)
     for value in regex_dict.values():
-        schedule_dict[formatted_time[counter]] = value
-        counter += 1
-
-
+        if not counter > len(formatted_time) - 1:
+            schedule_dict[formatted_time[counter]] = value
+            counter += 1
+    table = create_table(schedule_dict)
+    return table
 
 
 def get_formatted_time(time_list) -> list:
@@ -47,30 +48,37 @@ def get_formatted_time(time_list) -> list:
         elif hours < 12:
             time.append(f"{hours}:{minutes:02} AM")
     return time
-print(get_formatted_time(['00:00', '00:12', '07:01', '08:01', '08:08', '10:00', '11:00', '18:19', '24:01']))
 
-if __name__ == '__main__':
-    print(create_schedule_string("wat 11:00 teine tekst 11:0 jah ei 10:00 pikktekst "))
-    print(create_schedule_string(
-        """A 11:00 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed euismod nibh, non vehicula libero. Fusce ac eros
-     lectus. Pellentesque interdum nisl sem, eget facilisis mauris malesuada eget. Nullam 10:0 a bibendum enim. Praesent dictum
-     ante eget turpis tempor, porta placerat dolor ultricies. Mauris quis dui porttitor, ultrices turpis vitae, pulvinar nisl.
-     Suspendisse potenti. Ut nec cursus sapien, convallis sagittis purus. Integer mollis nisi sed fermentum efficitur.
-     Suspendisse sollicitudin sapien dui, vitae tempus lacus elementum ac. Curabitur id purus diam. 24:01 Donec blandit,
-     est nec semper convallis, arcu libero lacinia ex, eu placerat risus est non tellus.
 
-    Orci varius natoque penatibus et magnis dis 0:12 parturient montes, nascetur ridiculus mus. Curabitur pretium at metus
-    eget euismod. Nunc sit amet fermentum urna. Maecenas commodo ex turpis, et malesuada tellus sodales non. Fusce elementum
-     eros est. Phasellus nibh magna, tincidunt eget magna nec, rhoncus lobortis dui. Sed fringilla risus a justo tincidunt,
-     in tincidunt urna interdum. Morbi varius lobortis tellus, vitae accumsan justo commodo in. 12:001 Nullam eu lorem leo.
-     Vestibulum in varius magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-      0:00 Aliquam ac velit sit amet nunc dictum aliquam pulvinar at enim. Nulla aliquam est quis sem laoreet, eu venenatis
-      risus hendrerit. Donec ac enim lobortis, bibendum lacus quis, egestas nisi.
+def get_table_sizes(schedule_dict: dict):
+    """Get the maximum sizes for table."""
+    key_len = 0
+    value_len = 0
+    for key in schedule_dict.keys():
+        if len(key) > key_len:
+            key_len = len(key)
+    for value in schedule_dict.values():
+        if len(value) > key_len:
+            value_len = len(value)
+    return key_len, value_len
 
-    08:01 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed euismod nibh, non vehicula libero. Fusce ac eros
-     lectus. Pellentesque interdum nisl sem, eget facilisis mauris malesuada eget. Nullam 18:19 a bibendum enim. Praesent
-     dictum ante eget turpis tempor, 00:0 porta placerat dolor ultricies. Mauris quis dui porttitor, ultrices turpis vitae,
-     pulvinar nisl. Suspendisse potenti. Ut nec cursus sapien, convallis sagittis purus. 8:8 Integer mollis nisi sed fermentum
-      efficitur. Suspendisse sollicitudin sapien dui, vitae tempus lacus elementum ac. Curabitur id 18:19 purus
-      diam. 18:19 Donec blandit, est nec semper convallis, arcu 7.01 libero lacinia ex, eu placerat risus est non tellus."""))
-    create_schedule_file("schedule_input.txt", "schedule_output.txt")
+
+def create_table(schedule_dict: dict):
+    """Create table."""
+    if len(schedule_dict) > 0:
+        sizes = get_table_sizes(schedule_dict)
+        width = sizes[0] + sizes[1]
+        table = f"{'-' * (width + 7)}\n"
+        table += f"| {'time':>{sizes[0]}} | {'items':{sizes[1]}} |\n"
+        table += f"{'-' * (width + 7)}\n"
+        for key in schedule_dict.keys():
+            table += f"| {key:>{sizes[0]}} | {schedule_dict[key]:{sizes[1]}} |\n"
+        table += f"{'-' * (width + 7)}\n"
+    else:
+        width = len("No items found")
+        table = f"{'-' * (width + 4)}\n"
+        table += f"|  time | items  |\n"
+        table += f"{'-' * (width + 4)}\n"
+        table += f"| No items found |\n"
+        table += f"{'-' * (width + 4)}\n"
+    return table
