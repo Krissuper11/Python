6c49ff04db67799926272b29ae4a5ac1007e0128
Kristofer Putsinin <kriput@ttu.ee>
2021-10-13 11:04:23 +0300

    My commit

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -187,13 +187,19 @@ def merge_dates_and_towns_into_csv(dates_file: str, towns_file: str, csv_output:
     for line in towns_list:
         if ":" in line:
             index = line.find(":")
-            person_dict[line[:index]].insert(0, line[index + 1:])
+            if line[:index] in person_dict:
+                person_dict[line[:index]].insert(0, line[index + 1:])
+            else:
+                person_dict[line[:index]] = [(line[index + 1:]), "-"]
         else:
             person_dict[line].insert(0, "-")
     for key, value in person_dict.items():
+        if len(value) < 2:
+            value.insert(0, "-")
         person_list.append([key, value[0], value[1]])
     with open(csv_output, "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=",")
         csv_writer.writerow(["name", "town", "date"])
         for row in person_list:
             csv_writer.writerow(row)
+print(merge_dates_and_towns_into_csv("C:/Users/krist/Downloads/dates.txt", "C:/Users/krist/Downloads/towns.txt", "C:/Users/krist/Downloads/test.txt"))
