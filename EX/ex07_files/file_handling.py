26681a356a658f55ed61ed4dda9d9eac910e0e57
Kristofer Putsinin <kriput@ttu.ee>
2021-10-17 18:28:07 +0300

    My commit

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -456,7 +456,7 @@ def read_people_data(directory: str) -> dict:
     :return: Dictionary with id as keys and data dictionaries as values.
     """
     import re
-    file_names = re.findall(r"[\S]+.csv", directory)
+    file_names = re.findall(r"[\S]+\.csv", directory)
     list_with_dicts = []
     collected_data = {}
     people_data = {}
@@ -492,7 +492,21 @@ def read_people_data(directory: str) -> dict:
             if key not in people_data:
                 people_data[key] = value[i]
     return people_data
+print(read_people_data("""File: C:/Users/krist/Downloads/test.csv
+id,name
+1,john
+2,mary
+3,john
 
+File: C:/Users/krist/Downloads/dates.csv
+id,birth
+1,01.01.2001
+2,05.06.1990
+
+File: C:/Users/krist/Downloads/towns.csv
+id,death
+2,01.02.2020
+1,-"""))
 
 def generate_people_report(person_data_directory: str, report_filename: str) -> None:
     """
