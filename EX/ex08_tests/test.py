6b09a474922ff3bd6bbdf2c97907896db8b71fab
Kristofer Putsinin <kriput@ttu.ee>
2021-10-20 23:26:43 +0300

    My commit

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -4,7 +4,7 @@ import solution
 
 def test_part1_int_correct_len():
     element_list = ["string", "int", "float", "list", "tuple", "dict", "set"]
-    counter = 1
+    counter = 2
     for element in element_list:
         res = solution.generate_list(counter, element)
         assert len(res) == counter
@@ -29,14 +29,12 @@ def test_part1_correct_data_type():
 
 def test_part2_correct_len():
     element_list = ["string", "int", "float", "list", "tuple", "dict", "set"]
-    counter = 0
-    counter_max = 0
+    input_min = 0
+    input_max = 0
     for element in element_list:
-        input_min = counter
-        input_max = counter_max
         assert len(solution.generate_combined_list([(input_min, element), (input_max, element)])) == input_max
-        counter += 1
-        counter_max += 2
+        input_min += 1
+        input_max += 2
 
 
 def test_part2_correct_data_type_single():
@@ -46,3 +44,30 @@ def test_part2_correct_data_type_single():
         output_list = solution.generate_combined_list([(4, element_list[i])])
         for element in output_list:
             assert isinstance(element, data_type)
+
+
+def test_part3_correct_len():
+    element_list = ["string", "int", "float"]
+    input_min = 0
+    input_max = 0
+    for element in element_list:
+        assert len(solution.generate_combined_list_unique([(input_min, element), (input_max, element)])) == input_max
+        input_min += 1
+        input_max += 2
+
+
+def test_part3_correct_data_type_single():
+    element_list = ["string", "int", "float"]
+    datatype_list = [str, int, float]
+    for i, data_type in enumerate(datatype_list):
+        output_list = solution.generate_combined_list_unique([(4, element_list[i])])
+        for element in output_list:
+            assert isinstance(element, data_type)
+
+
+def test_part3_unique_single():
+    element_list = ["string", "int", "float"]
+    for data_type in element_list:
+        output_list = solution.generate_combined_list_unique([(4, data_type)])
+        for i in range(1, len(output_list)):
+            assert output_list[i] != output_list[i - 1]
