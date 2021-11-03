9a603c1e169764481678c6970197ed13848e8b77
Kristofer Putsinin <kriput@ttu.ee>
2021-11-03 15:21:02 +0200

    My commit

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,6 +1,35 @@
+"""WAT."""
+
+
 def function_a(a):
+    """Return a"""
     return 5
 
 
 def function_b(b):
+    """Return b + 5."""
+    return b + 5
+
+
+def function_c(c):
+    pass
+
+
+def function_d(c):
+    pass
+
+
+def function_e(c):
+    pass
+
+
+def function_f(c):
+    pass
+
+
+def function_g(c):
+    pass
+
+
+def function_h(c):
     pass
