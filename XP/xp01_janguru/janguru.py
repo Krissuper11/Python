ad7fef576450eda9316730d6c54d3a0bbf764d9b
Kristofer Putsinin <kriput@ttu.ee>
2021-11-07 16:04:20 +0200

    My commit

---
+++ XP/xp01_janguru/janguru.py
@@ -0,0 +1,48 @@
+"""XP01."""
+import math
+
+
+def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
+
+    """Calculate the meeting position of 2 jangurus.
+    @:param pos1: position of first janguru
+    @:param jump_distance1: jump distance of first janguru
+    @:param sleep1: sleep time of first janguru
+    @:param pos2: position of second janguru
+    @:param jump_distance2: jump distance of second janguru
+    @:param sleep2: sleep time of second janguru
+
+    @:return positions where jangurus first meet
+    """
+    if pos1 == pos2:
+        return pos1
+    time_move = math.gcd(sleep1, sleep2)
+    time1 = 0
+    while time1 <= sleep1:
+        if time1 == sleep1:
+            pos1 += jump_distance1
+        if pos1 == pos2:
+            return pos1
+        time1 += time_move
+    time2 = 0
+    while time2 <= sleep1:
+        if time2 == sleep2:
+            pos2 += jump_distance2
+        if pos1 == pos2:
+            return pos1
+        time2 += time_move
+    try:
+        return meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2)
+    except RecursionError:
+        return -1
+
+
+
+if __name__ == "__main__":
+    print(meet_me(1, 2, 1, 2, 1, 1))  # => 3
+    print(meet_me(1, 2, 3, 4, 5, 5))  # => -1
+    print(meet_me(10, 7, 7, 5, 8, 6))  # => 45
+    # print(meet_me(100, 7, 4, 300, 8, 6))  # => 940
+    # print(meet_me(1, 7, 1, 15, 5, 1))  #  => 50
+    # print(meet_me(0, 1, 1, 1, 1, 1))  # => -1
+    # print(meet_me(1, 2, 1, 1, 3, 1))  # => -1
