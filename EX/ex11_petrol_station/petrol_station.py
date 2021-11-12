d6d2fd86dbe4a3eff5a9f7885a6792d05089a6b4
Kristofer Putsinin <kriput@ttu.ee>
2021-11-12 23:39:29 +0200

    My commit

--- EX/ex11_petrol_station/petrol_station.py
+++ EX/ex11_petrol_station/petrol_station.py
@@ -373,7 +373,7 @@ class PetrolStation:
         if fuel in self.__fuel_stock and quantity <= self.__fuel_stock[fuel]:
             self.__fuel_stock[fuel] -= quantity
         else:
-            raise RuntimeError()
+            raise RuntimeError(self.get_fuel_dict())
 
     def remove_items(self, item: ShopItem, quantity: float):
         """
@@ -438,11 +438,7 @@ class PetrolStation:
         if len(items_to_sell) != 0:
             for order in items_to_sell:
                 if isinstance(order[0], Fuel):
-                    try:
-                        self.remove_fuel(order[0], order[1])
-                    except RuntimeError:
-                        self.__shop_item_stock = self.get_shop_item_dict()
-                        raise RuntimeError
+                    self.remove_fuel(order[0], order[1])
                 elif isinstance(order[0], ShopItem):
                     self.remove_items(order[0], order[1])
                 if order[0] not in order_dict:
