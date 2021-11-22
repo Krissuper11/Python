4fec071eedf4b034ff1969c48055b157fe099eca
Kristofer Putsinin <kriput@ttu.ee>
2021-11-22 16:15:30 +0200

    My commit

---
+++ EX/ex13_api/google_api.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+def get_links_from_spreadsheet(id: str, token: str) -> list:
+    """
+    Return a list of strings from the first column of a Google Spreadsheet with the given ID.
+    Example input with https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
+        get_links_from_spreadsheet('1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms', 'token.json')
+
+    Returns
+        ['Student Name', 'Alexandra', 'Andrew', 'Anna', 'Becky', ... and so on from the first column]
+    """
+    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    creds = Credentials.from_authorized_user_file(token, scopes)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=id,
+                                range="Class Data!A1:A31").execute()
+    values = result.get('values', [])
+    return [row[0] for row in values]
