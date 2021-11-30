131e8308fc1d46dd4acd61fcfc47aba9e24bc442
Kristofer Putsinin <kriput@ttu.ee>
2021-11-30 12:30:44 +0200

    My commit

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -2,12 +2,11 @@
 from FollowerBot import FollowerBot
 
 
-def run(robot: FollowerBot):
+def test_run(robot: FollowerBot):
     """
     Make the robot move, doesnt matter how much, just as long as it has moved from the starting position.
     :param FollowerBot robot: instance of the robot that you need to make move
     """
-
     robot = FollowerBot()
     robot.set_wheels_speed(30)
     robot.sleep(2)
