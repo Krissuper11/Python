986f21d40124a8f7c224fc256560faf87346bef4
Kristofer Putsinin <kriput@ttu.ee>
2021-12-04 13:58:27 +0200

    My commit

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -59,7 +59,7 @@ def follow_the_line(robot: FollowerBot):
             robot.set_wheels_speed(0)
             robot.done()
             break
-
+    robot.set_wheels_speed(0)
 
 def the_true_follower(robot: FollowerBot):
     """
