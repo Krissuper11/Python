06abec33431aa185eba7e4f76c713295d47d06d6
Kristofer Putsinin <kriput@ttu.ee>
2021-12-08 15:18:08 +0200

    My commit

--- EX/ex00_intro/hello.py
+++ EX/ex00_intro/hello.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+import random
+n = 3
+list2 = ["danja", "kristina", "kristofer", "olja"]
+list3 = []
+while True:
+    input1 = input("Name: ")
+    if input1 == "end":
+        print(set(list3))
+    index2 = random.randint(0, n)
+    person2 = list2[index2]
+    list3.append(person2)
+    if person2 != input1:
+        del list2[index2]
+        print(person2)
+        n -= 1
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
