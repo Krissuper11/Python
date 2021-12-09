b3b464fe95bd19e4bf321c1426aa6da573e68d37
Kristofer Putsinin <kriput@ttu.ee>
2021-12-09 14:02:47 +0200

    My commit

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -56,13 +56,13 @@ def follow_the_line(robot: FollowerBot):
             robot.set_wheels_speed(45)
             robot.sleep(0.01)
             robot_stop = 0
-        elif sum(robot.get_left_line_sensors()) < sum(robot.get_right_line_sensors()):
+        elif sum(robot.get_left_line_sensors()) < sum(robot.get_right_line_sensors()) and robot_stop != 1:
             robot.set_right_wheel_speed(70)
             robot.set_left_wheel_speed(100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
             robot.sleep(0.01)
-        elif sum(robot.get_left_line_sensors()) > sum(robot.get_right_line_sensors()):
+        elif sum(robot.get_left_line_sensors()) > sum(robot.get_right_line_sensors()) and robot_stop !=1:
             robot.set_left_wheel_speed(70)
             robot.set_right_wheel_speed(100)
             robot.sleep(0.01)
@@ -74,7 +74,7 @@ def follow_the_line(robot: FollowerBot):
         elif sum(robot.get_line_sensors()) == 6144:
             robot.set_right_wheel_speed(80)
             robot.set_left_wheel_speed(-80)
-            robot.sleep(0.2)
+            robot.sleep(0.15)
             robot_stop += 1
 
     robot.done()
