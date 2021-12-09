a37743e70447c8ebc7063454ad704a07d5331c3a
Kristofer Putsinin <kriput@ttu.ee>
2021-12-09 13:06:36 +0200

    My commit

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -66,8 +66,8 @@ def follow_the_line(robot: FollowerBot):
             robot.done()
             break
         elif sum(robot.get_line_sensors()) == 6144:
-            robot.set_right_wheel_speed(-25)
-            robot.set_left_wheel_speed(50)
+            robot.set_right_wheel_speed(50)
+            robot.set_left_wheel_speed(-25)
             robot.sleep(0.1)
             robot_stop += 1
 
