b28abbe496d2ed538a1c178b4ef08f4f274dc6bd
Kristofer Putsinin <kriput@ttu.ee>
2021-12-09 13:54:41 +0200

    My commit

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -47,28 +47,28 @@ def follow_the_line(robot: FollowerBot):
         robot.sleep(0.01)
         robot.set_wheels_speed(0)
     while True:
-        if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0:
-            robot.set_wheels_speed(45)
-            robot.sleep(0.01)
-            robot_stop = 0
-        elif robot_stop == 1:
+        if robot == 2:
             robot.set_right_wheel_speed(80)
             robot.set_left_wheel_speed(-80)
             robot.sleep(0.15)
             robot_stop += 1
-        elif sum(robot.get_left_line_sensors()) < sum(robot.get_right_line_sensors()) and robot_stop != 2:
+        elif robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0:
+            robot.set_wheels_speed(45)
+            robot.sleep(0.01)
+            robot_stop = 0
+        elif sum(robot.get_left_line_sensors()) < sum(robot.get_right_line_sensors()):
             robot.set_right_wheel_speed(70)
             robot.set_left_wheel_speed(100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
             robot.sleep(0.01)
-        elif sum(robot.get_left_line_sensors()) > sum(robot.get_right_line_sensors()) and robot_stop != 2:
+        elif sum(robot.get_left_line_sensors()) > sum(robot.get_right_line_sensors()):
             robot.set_left_wheel_speed(70)
             robot.set_right_wheel_speed(100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
             robot.sleep(0.01)
-        elif robot_stop == 2:
+        elif robot_stop == 3:
             robot.done()
             break
         elif sum(robot.get_line_sensors()) == 6144:
