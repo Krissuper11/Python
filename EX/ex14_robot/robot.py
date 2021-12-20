d533fe391f608e3f9176af2462183602a3302a2e
Kristofer Putsinin <kriput@ttu.ee>
2021-12-20 20:28:54 +0200

    My commit

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -107,7 +107,7 @@ def the_true_follower(robot: FollowerBot):
             robot.set_wheels_speed(0)
             robot.sleep(0.01)
         elif sum(robot.get_left_line_sensors()) > sum(robot.get_right_line_sensors()):
-            robot.set_left_wheel_speed(70)
+            robot.set_left_wheel_speed(50)
             robot.set_right_wheel_speed(100)
             robot.sleep(0.02)
             robot.set_wheels_speed(0)
