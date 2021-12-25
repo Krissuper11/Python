387b8a1e5789961c201de3c671e6e1cde5ed1e8a
Kristofer Putsinin <kriput@ttu.ee>
2021-12-25 22:40:04 +0200

    My commit

--- EX/ex15_santas_workshop/santas_workshop.py
+++ EX/ex15_santas_workshop/santas_workshop.py
@@ -1,5 +1,13 @@
 """EX 15."""
+import binascii
 import csv
+import json
+import urllib.request
+import urllib.parse
+import urllib.error
+from copy import deepcopy
+import re
+import base64
 
 
 class Info:
@@ -37,8 +45,10 @@ class Info:
         for child in self.nice_children:
             if child.name == name:
                 return child
-            else:
-                return None
+        for child in self.naughty_children:
+            if child.name == name:
+                return child
+        return None
 
 
 class Child:
@@ -51,10 +61,287 @@ class Child:
         self.country = country
         self.presents = []
 
+    def __repr__(self):
+        """Name."""
+        return self.name
+
     def add_presents(self, wish_list: list):
         """Add presents to child from file."""
         self.presents += wish_list
 
 
+class Factory:
+    """Santa's factory."""
+
+    def __init__(self, info: Info):
+        """Factory constructor."""
+        self.completed_presents = {}
+        self.info = info
+        self.presents = []
+
+    def produce_presents(self):
+        """Produce presents."""
+        children_list = deepcopy(sorted(self.info.nice_children, key=lambda x: len(x.presents), reverse=True))
+        counter = 0
+        time_count = 0
+        try:
+            max_counter = len(children_list[0].presents)
+        except IndexError:
+            return None
+        while counter <= max_counter:
+            for i, child in enumerate(self.info.nice_children):
+                if time_count < 525600:
+                    time_count += self.add_gift_info(child, counter)
+                elif 525600 < time_count < max_counter:
+                    counter += max_counter
+            if counter == 0:
+                for child in self.info.naughty_children:
+                    if time_count < 525600:
+                        time_count += self.add_gift_info(child, counter)
+                    elif 8760 < time_count < max_counter:
+                        counter += max_counter
+
+    def add_gift_info(self, child, counter):
+        """Add information about gift."""
+        try:
+            query = child.presents[counter]
+            gift = deepcopy(self.find_gift_in_list(query))
+            if gift is None:
+                page = urllib.parse.urlencode({"name": query})
+                response = urllib.request.urlopen(f"http://api.game-scheduler.com:8089/gift?{page}")
+                gift_info = json.loads(response.read().decode())
+                gift = Gift(gift_info["gift"], child.name, child.country, gift_info["weight_in_grams"], gift_info["production_time"])
+                self.presents.append(gift)
+            else:
+                gift.child_name = child.name
+                gift.country = child.country
+            if child.name not in self.completed_presents:
+                self.completed_presents[child.name] = [gift]
+            else:
+                self.completed_presents[child.name].append(gift)
+            return gift.time
+        except IndexError:
+            return 0
+        except urllib.error.HTTPError:
+            return 0
+
+    def find_gift_in_list(self, gift_name):
+        """Find gift."""
+        for gift in self.presents:
+            if gift.gift_name == gift_name:
+                return gift
+        return None
+
+
+class Gift:
+    """Gift."""
+
+    def __init__(self, gift_name, child_name, country, weight, time):
+        """Present constructor."""
+        self.gift_name = gift_name
+        self.child_name = child_name
+        self.country = country
+        self.weight = weight
+        self.time = time
+
+    def __repr__(self):
+        """Name."""
+        return self.gift_name
+
+
+class FlightPlan:
+    """Prepare for departure."""
+
+    def __init__(self, factory: Factory):
+        """FlightPlan constructor."""
+        self.factory = factory
+        self.flight_plan = {}
+        self.ready_transport = {}
+
+    def create_flight_plan(self):
+        """Create flight plan."""
+        for present_list in self.factory.completed_presents.values():
+            if present_list[0].country not in self.flight_plan:
+                self.flight_plan[present_list[0].country] = [present_list]
+            else:
+                self.flight_plan[present_list[0].country].append(present_list)
+
+    def prepare_transport(self):
+        """Prepare transport."""
+        for country, country_gift_list in self.flight_plan.items():
+            for gift_list in country_gift_list:
+                for gift in gift_list:
+                    added_check = False
+                    if country not in self.ready_transport:
+                        self.ready_transport[country] = [Transport(country)]
+                    for transport in self.ready_transport[country]:
+                        if gift.weight <= transport.space_left and not added_check:
+                            transport.add_gifts(gift)
+                            transport.space_left -= gift.weight
+                            added_check = True
+                            continue
+                    if not added_check:
+                        transport = Transport(country)
+                        self.ready_transport[country].append(transport)
+                        transport.add_gifts(gift)
+        for transport_list in self.ready_transport.values():
+            for transport in transport_list:
+                transport.create_delivery_note()
+
+
+class Transport:
+    """Transport."""
+
+    def __init__(self, country):
+        self.country = country
+        self.space_left = 50000
+        self.gifts = {}
+        self.delivery_note = ""
+
+    def __repr__(self):
+        return self.country
+
+    def add_gifts(self, gift):
+        """Add gifts."""
+        if gift.child_name not in self.gifts:
+            self.gifts[gift.child_name] = [gift]
+        else:
+            self.gifts[gift.child_name].append(gift)
+
+    def create_delivery_note(self):
+        """Create delivery note."""
+        self.delivery_note += \
+            """                          DELIVERY ORDER
+                                                          _v
+                                                     __* (__)
+             ff     ff     ff     ff                {\/ (_(__).-.
+      ff    <_\__, <_\__, <_\__, <_\__,      __,~~.(`>|-(___)/ ,_)
+    o<_\__,  (_ ff ~(_ ff ~(_ ff ~(_ ff~~~~~@ )\/_;-"``     |
+      (___)~~//<_\__, <_\__, <_\__, <_\__,    | \__________/|
+      // >>     (___)~~(___)~~(___)~~(___)~~~~\\\\_/_______\_//
+                // >>  // >>  // >>  // >>     `'---------'` 
+
+FROM: NORTH POLE CHRISTMAS CHEER INCORPORATED
+TO: """
+        self.delivery_note += self.country.upper() + "\n" + "\n"
+        gift_dict_by_name = sorted(self.gifts, key=lambda x: len(x), reverse=True)
+        max_name = len(gift_dict_by_name[0])
+        if max_name < 4:
+            max_name = 4
+        max_gift_length = 0
+        for gift_list in self.gifts.values():
+            gift_list_length = len(str(gift_list))
+            if gift_list_length > max_gift_length:
+                max_gift_length = gift_list_length - 2
+        if max_gift_length < 5:
+            max_gift_length = 5
+        self.delivery_note += f"//={'=' * max_name}=[]={'=' * max_gift_length}=[]{'=' * 18}\\\\\n"
+        self.delivery_note += f"|| {' ' * ((max_name - 4) // 2)}Name{' ' * ((max_name - 4) - (max_name - 4) // 2)}" \
+                              f" || {' ' * ((max_gift_length - 5) // 2)}Gifts" \
+                              f"{' ' * ((max_gift_length - 5) - (max_gift_length - 5) // 2)} || Total Weight(kg) ||\n"
+        self.delivery_note += f"|]={'=' * max_name}=[]={'=' * max_gift_length}=[]{'=' * 18}[|\n"
+        for name, gift_list in self.gifts.items():
+            gift_list_length = len(str(gift_list)) - 2
+            weight_sum = round(sum([int(gift.weight) / 1000 for gift in gift_list]), 2)
+            self.delivery_note += f"|| {name}{' ' * (max_name - len(name))} || {str(gift_list)[1:-1]}" \
+                                  f"{' ' * (max_gift_length - gift_list_length)} || " \
+                                  f"{' ' * (16 - len(str(weight_sum)))}{weight_sum} ||\n"
+        self.delivery_note += f"\\\\={'=' * max_name}=[]={'=' * max_gift_length}=[]{'=' * 18}//"
+
+
+class PostOffice:
+    """Santa's Post office."""
+
+    def __init__(self, info):
+        """Constructor."""
+        self.info = info
+        self.name = ""
+        self.name_check = []
+
+    def read_letter(self, test_parameter: str = None):
+        """
+        Read letter.
+
+        Test parameter is used only while testing to give exact string to read.
+        """
+        for i in range(50):
+            counter = 0
+            if test_parameter is not None:
+                gift_info = test_parameter
+            else:
+                response = urllib.request.urlopen("http://api.game-scheduler.com:8089/letter")
+                gift_info = json.loads(response.read().decode())
+            task_completed = self.set_data(gift_info, counter)
+            if task_completed is False and counter == 0:
+                counter += 1
+                gift_info_encoded = self.encode(gift_info, -4)
+                task_completed = self.set_data(gift_info_encoded, counter)
+            if task_completed is False and counter == 1:
+                counter += 1
+                try:
+                    gift_info_encoded = base64.b64decode(gift_info).decode("UTF-8")
+                    self.set_data(gift_info_encoded, counter)
+                except UnicodeDecodeError:
+                    pass
+                except binascii.Error:
+                    pass
+            if test_parameter is not None:
+                break
+
+    def set_data(self, gift_info, counter):
+        """Set data."""
+        name = None
+        result = re.findall(r"(?<=[Ii] want ).+(?=\.)|(?<=[Ii] wish for ).+(?=\.)|(?<=wishlist: ).+(?=\.)",
+                            gift_info)
+        res = re.finditer(r"(?<=\n)([\w ]+), ([\w ]+)$", gift_info)
+        for match in res:
+            name = match.group(1)
+            country = match.group(2)
+        if counter == 1 and name is not None:
+            name = name.title()
+            country = country.title()
+            if "of" in country:
+                country[country.index("of"):country.index("of") + 2] = "of"
+        if len(result) == 0 or name is None:
+            return False
+        if name not in self.name_check:
+            self.name_check.append(name)
+            if self.info.find_children_in_list(name) is None:
+                child = Child(name, "nice", country)
+                self.info.nice_children.append(child)
+            else:
+                child = self.info.find_children_in_list(name)
+            for gift_list in result:
+                gift_str = gift_list.split(", ")
+                for gift in gift_str:
+                    if gift.lower() not in [gift1.lower() for gift1 in child.presents]:
+                        child.presents.append(gift)
+        return True
+
+    def encode(self, message: str, shift: int) -> str:
+        """Encode a message using a Caesar cipher."""
+        new_letter = ""
+        for i in range(len(message)):
+            if message[i] == "n" and message[i - 1] == "\\":
+                continue
+            elif not message[i].isalpha():
+                new_letter += message[i]
+            elif message[i].isalpha():
+                index = ord(message[i]) + shift
+                index -= 26 * ((index - 97) // 26)
+                new_letter += chr(index)
+            else:
+                pass
+        return new_letter
+
+
 if __name__ == '__main__':
-    info = Info("ex15_naughty_list.csv", "ex15_nice_list.csv", "ex15_wish_list.csv")
+    info = Info("test_empty.csv", "test_empty.csv", "ex15_wish_list.csv")
+    factory = Factory(info)
+    PostOffice(info).read_letter("SGVsbG8sIFNhbnRhIQoKSSBoYXZlIGJlZW4gdmVyeSBuaWNlIHRvIG15IGZhbWlseSBhbmQgZnJpZW5kcywgYW5kIGV2ZW4gc29tZSBwZW9wbGUgd2hvIGhhdmUgbm90IGJlZW4gdmVyeSBuaWNlIHRvIG1lLCBsaWtlIG91ciBuZWlnaGJvciB3aG8geWVsbHMgYXQgbWUgd2hlbiBJIHBsYXkgd2l0aCB0aGUgd2F0ZXJob3NlLgoKVGhpcyB5ZWFyLCBJIHdhbnQgUGluayB0cmljeWNsZS4KCkNhbid0IHdhaXQgdG8gc2VlIHlvdSwKQnJlbm5hbiwgUHVlcnRvIFJpY28=")
+    factory.produce_presents()
+    flight = FlightPlan(factory)
+    flight.create_flight_plan()
+    flight.prepare_transport()
+    print(flight.ready_transport)
+    print(flight.ready_transport["Puerto Rico"][0].delivery_note)
---
+++ EX/ex15_santas_workshop/workshop_test.py
@@ -0,0 +1,235 @@
+import santas_workshop
+
+info = santas_workshop.Info("ex15_naughty_list.csv", "ex15_nice_list.csv", "ex15_wish_list.csv")
+factory = santas_workshop.Factory(info)
+factory.produce_presents()
+flight = santas_workshop.FlightPlan(factory)
+flight.create_flight_plan()
+flight.prepare_transport()
+
+
+def test_add_to_list():
+    """Test that all children are added."""
+    assert len(info.nice_children) == 291
+    assert len(info.naughty_children) == 108
+    nice_children = ["Stacy", "Libby", "Dillion", "Selena", "Mimi"]
+    nice_names = [child.name for child in info.nice_children]
+    naughty_children = ["Bailey", "Tanya", "Sylvia", "Austin", "Mel"]
+    naughty_names = [child.name for child in info.naughty_children]
+    for child_name in nice_children:
+        assert child_name in nice_names and child_name not in naughty_names
+    for child_name in naughty_children:
+        assert child_name in naughty_names and child_name not in nice_children
+
+
+def test_correct_countries():
+    """Test that country is read correctly."""
+    nice_dict = {}
+    naughty_dict = {}
+    nice_children = {"Stacy": "United Kingdom", "Libby": "United Kingdom",
+                     "Dillion": "Germany", "Selena": "Australia", "Mimi": "Canada"}
+    for child in info.nice_children:
+        nice_dict[child.name] = child.country
+    naughty_children = {"Bailey": "Peru", "Tanya": "United Kingdom", "Sylvia": "United Kingdom",
+                        "Austin": "Peru", "Mel": "Sweden"}
+    for child in info.naughty_children:
+        naughty_dict[child.name] = child.country
+    for name in nice_children:
+        assert nice_dict[name] == nice_children[name]
+    for name in naughty_children:
+        assert naughty_dict[name] == naughty_children[name]
+
+
+def test_presents_added_correctly():
+    """Test that presents are added correctly from wish list."""
+    nice_dict = {}
+    naughty_dict = {}
+    nice_children = {"Stacy": ["Polyhedral dice set", "Wall-mount diamond pickaxe", "500 TikTok followers"],
+                     "Libby": ["Zebra Jumpy", "Princess dress", "Lego death star"],
+                     "Dillion": ["Xbox Series X"], "Selena": ["Baby born doll", "Toy kitchen set"],
+                     "Mimi": ["Toy dinosaur play set"]}
+    for child in info.nice_children:
+        nice_dict[child.name] = child.presents
+    naughty_children = {
+        "Bailey": ["Magic: The Gathering Commander Legends booster box", "Ninja Turtles backpack", "Zebra Jumpy"],
+        "Tanya": ["Nintendo Switch", "Frozen Olaf plush toy"], "Sylvia": ["Tablet computer"],
+        "Austin": ["Carbon fiber road bike"], "Mel": ["Lego Grogu"]}
+    for child in info.naughty_children:
+        naughty_dict[child.name] = child.presents
+    for name in nice_children:
+        assert nice_dict[name] == nice_children[name]
+    for name in naughty_children:
+        assert naughty_dict[name] == naughty_children[name]
+
+
+def test_gift_created_correctly():
+    """Test that gift has correct information (name, weight)"""
+    test_list = {"Stacy": factory.completed_presents["Stacy"], "Libby": factory.completed_presents["Libby"]}
+    gifts = {"Stacy": {"Polyhedral dice set": 10, "Wall-mount diamond pickaxe": 695, "500 TikTok followers": 1},
+             "Libby": {"Zebra Jumpy": 1337, "Princess dress": 278, "Lego death star": 2000}}
+    name_counter = 0
+    for key, value in test_list.items():
+        for gift in value:
+            assert gift.gift_name in gifts[key].keys()
+            assert gift.child_name == list(gifts.keys())[name_counter]
+            assert gift.weight == gifts[key][gift.gift_name]
+        name_counter += 1
+
+
+def test_good_children_get_all():
+    """Test that good children get all what they wished."""
+    nice_children = {"Stacy": 3, "Libby": 3, "Dillion": 1, "Selena": 2, "Mimi": 1}
+    for child in nice_children:
+        assert len(factory.completed_presents[child]) == nice_children[child]
+
+
+def test_naughty_get_only_one():
+    """Test that naughty children get no more than one gift."""
+    naughty_children = ["Bailey", "Tanya", "Sylvia", "Jay", "Danni"]
+    for child in naughty_children:
+        assert len(factory.completed_presents[child]) == 1
+
+
+def test_correct_flight_plan():
+    """Test correct flight plan."""
+    correct_num = len([child.country for child in info.nice_children if child.country == "Estonia"])
+    correct_num += len([child.country for child in info.naughty_children if child.country == "Estonia"])
+    assert len(flight.flight_plan["Estonia"]) == correct_num
+    correct_num = len([child.country for child in info.nice_children if child.country == "United Kingdom"])
+    correct_num += len([child.country for child in info.naughty_children if child.country == "United Kingdom"])
+    assert len(flight.flight_plan["United Kingdom"]) == correct_num
+
+
+def test_optimized_transport():
+    """Test correct optimization."""
+    # Test if weight is exactly 50kg.
+    info = santas_workshop.Info("nice_test_transport.csv", "naughty_test_transport.csv", "wish_list_test.csv")
+    factory = santas_workshop.Factory(info)
+    factory.produce_presents()
+    flight = santas_workshop.FlightPlan(factory)
+    flight.create_flight_plan()
+    flight.prepare_transport()
+    assert len(flight.ready_transport["Estonia"]) == 1
+    # Test if over 50 and if first gift does not fit, but second fits
+    assert len(flight.ready_transport["Finland"]) == 2
+    assert "Georgi" not in flight.ready_transport["Finland"][0].gifts \
+           and "Pekka" in flight.ready_transport["Finland"][0].gifts
+
+
+
+def test_delivery_note():
+    """Test delivery note."""
+    this_test_info = santas_workshop.Info("naughty_test.csv", "nice_test.csv", "wish_list_test.csv")
+    this_test_factory = santas_workshop.Factory(this_test_info)
+    this_test_factory.produce_presents()
+    this_test_flight = santas_workshop.FlightPlan(this_test_factory)
+    this_test_flight.create_flight_plan()
+    this_test_flight.prepare_transport()
+    assert this_test_flight.ready_transport["Estonia"][0].delivery_note == \
+        """                          DELIVERY ORDER
+                                                          _v
+                                                     __* (__)
+             ff     ff     ff     ff                {\/ (_(__).-.
+      ff    <_\__, <_\__, <_\__, <_\__,      __,~~.(`>|-(___)/ ,_)
+    o<_\__,  (_ ff ~(_ ff ~(_ ff ~(_ ff~~~~~@ )\/_;-"``     |
+      (___)~~//<_\__, <_\__, <_\__, <_\__,    | \__________/|
+      // >>     (___)~~(___)~~(___)~~(___)~~~~\\\\_/_______\_//
+                // >>  // >>  // >>  // >>     `'---------'` 
+
+FROM: NORTH POLE CHRISTMAS CHEER INCORPORATED
+TO: ESTONIA
+
+//==========[]========================================================[]==================\\\\
+||   Name   ||                         Gifts                          || Total Weight(kg) ||
+|]==========[]========================================================[]==================[|
+|| Smidl    || Cyberpunk 2077, Digital camera, Beyblade Burst spinner ||              1.2 ||
+|| Danja    || World of Warcraft: Shadowlands Collectors Edition      ||              0.3 ||
+|| Kristina || Ninja Turtles backpack, Tablet computer, VHS player    ||             1.98 ||
+\\\==========[]========================================================[]==================//"""
+    this_test_info = santas_workshop.Info("naughty_test.csv", "nice_test_note.csv", "wish_list_test.csv")
+    this_test_factory = santas_workshop.Factory(this_test_info)
+    this_test_factory.produce_presents()
+    this_test_flight = santas_workshop.FlightPlan(this_test_factory)
+    this_test_flight.create_flight_plan()
+    this_test_flight.prepare_transport()
+    assert this_test_flight.ready_transport["Estonia"][0].delivery_note == \
+        """                          DELIVERY ORDER
+                                                          _v
+                                                     __* (__)
+             ff     ff     ff     ff                {\/ (_(__).-.
+      ff    <_\__, <_\__, <_\__, <_\__,      __,~~.(`>|-(___)/ ,_)
+    o<_\__,  (_ ff ~(_ ff ~(_ ff ~(_ ff~~~~~@ )\/_;-"``     |
+      (___)~~//<_\__, <_\__, <_\__, <_\__,    | \__________/|
+      // >>     (___)~~(___)~~(___)~~(___)~~~~\\\\_/_______\_//
+                // >>  // >>  // >>  // >>     `'---------'` 
+
+FROM: NORTH POLE CHRISTMAS CHEER INCORPORATED
+TO: ESTONIA
+
+//======[]=========[]==================\\\\
+|| Name ||  Gifts  || Total Weight(kg) ||
+|]======[]=========[]==================[|
+|| Eva  || Crayons ||             0.05 ||
+\\\======[]=========[]==================//"""
+
+
+def test_post_office():
+    """Test that post office read letters correctly."""
+    # Test when no info about gifts
+    info = santas_workshop.Info("test_empty.csv", "test_empty.csv", "test_empty.csv")
+    santas_workshop.PostOffice(info).read_letter("Dear Santa!\n\nI saw an elf the other day, he was just making it out through the window when I spotted him\n\nSincerely yours,\nMaxwell, Puerto Rico")
+    assert len(info.nice_children) == 0
+
+    # Test when usual letter
+    info = santas_workshop.Info("test_empty.csv", "test_empty.csv", "test_empty.csv")
+    santas_workshop.PostOffice(info).read_letter("Hi, Santa!\n\nI saw an elf the other day, he was just making it out through the window when I spotted him\n\nThe following is my wishlist: Wall-mount diamond pickaxe, Magic: The Gathering Commander Legends booster box.\n\nThanks, Santa,\nAmelia, South Africa")
+    assert len(info.nice_children) == 1
+    child = info.nice_children[0]
+    assert child.name == "Amelia"
+    assert child.country == "South Africa"
+    assert child.presents == ["Wall-mount diamond pickaxe", "Magic: The Gathering Commander Legends booster box"]
+
+    # Test when Caesar cipher
+    info = santas_workshop.Info("test_empty.csv", "test_empty.csv", "test_empty.csv")
+    santas_workshop.PostOffice(info).read_letter("hiev qv erh qvw werxe!\n\nm lezi fiir vieppc kssh xlmw ciev.\n\nxli jsppsamrk mw qc amwlpmwx: tspev fiev tpywlmi, gcfivtyro 2077, omxxir tpywlmi.\n\nxlero csy,\nnegowsr, wsyxl ejvmge")
+    assert len(info.nice_children) == 1
+    child = info.nice_children[0]
+    assert child.name == "Jackson"
+    assert child.country == "South Africa"
+    assert child.presents == ["polar bear plushie", "cyberpunk 2077", "kitten plushie"]
+
+    # Test with base64
+    info = santas_workshop.Info("test_empty.csv", "test_empty.csv", "test_empty.csv")
+    santas_workshop.PostOffice(info).read_letter("SGVsbG8sIFNhbnRhIQoKSSBoYXZlIGJlZW4gdmVyeSBuaWNlIHRvIG15IGZhbWlseSBhbmQgZnJpZW5kcywgYW5kIGV2ZW4gc29tZSBwZW9wbGUgd2hvIGhhdmUgbm90IGJlZW4gdmVyeSBuaWNlIHRvIG1lLCBsaWtlIG91ciBuZWlnaGJvciB3aG8geWVsbHMgYXQgbWUgd2hlbiBJIHBsYXkgd2l0aCB0aGUgd2F0ZXJob3NlLgoKVGhpcyB5ZWFyLCBJIHdhbnQgUGluayB0cmljeWNsZS4KCkNhbid0IHdhaXQgdG8gc2VlIHlvdSwKQnJlbm5hbiwgUHVlcnRvIFJpY28=")
+    assert len(info.nice_children) == 1
+    child = info.nice_children[0]
+    assert child.name == "Brennan"
+    assert child.country == "Puerto Rico"
+    assert child.presents == ["Pink tricycle"]
+
+
+def test_bad_files():
+    """Test wrong files."""
+    # Test empty files
+    info = santas_workshop.Info("test_empty.csv", "test_empty.csv", "test_empty.csv")
+    factory = santas_workshop.Factory(info)
+    factory.produce_presents()
+    flight = santas_workshop.FlightPlan(factory)
+    flight.create_flight_plan()
+    flight.prepare_transport()
+    assert len(info.nice_children) == 0
+    assert len(info.naughty_children) == 0
+    assert len(factory.completed_presents) == 0
+    assert len(flight.ready_transport) == 0
+
+    # Test when no gift in database
+    info = santas_workshop.Info("test_empty.csv", "test_wrong_gift.csv", "wish_list_test.csv")
+    factory = santas_workshop.Factory(info)
+    factory.produce_presents()
+    flight = santas_workshop.FlightPlan(factory)
+    flight.create_flight_plan()
+    flight.prepare_transport()
+    assert len(info.nice_children) == 1
+    assert len(info.nice_children[0].presents) == 1
+    assert len(factory.completed_presents) == 0
+    assert len(flight.ready_transport) == 0
